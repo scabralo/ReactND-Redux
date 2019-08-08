31d67502edaf376aa11e2b8a6e963d5278e1f55e
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-08 18:02:39 -0400

    Adds listeners arr and subscribe method

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function createStore() {
   let state
+  let listeners = []
 
   const getState = () => state
 
+  const subscribe = (listener) => {
+    listeners.push(listener)
+  }
+
   return {
-    getState
+    getState,
+    subscribe
   }
 }
