7179ca2b926e2af480645a3fbf360d40066a8639
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-09 16:35:18 -0400

    Adds todo event to project

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
 function createStore() {
   let state
   let listeners = []
