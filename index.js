de13e9ab129ed8cb86afdceba30a55b1e14095ce
Sergio Cabral <sergioantonio60@gmail.com>
2019-08-10 15:59:55 -0400

    Adds goals reducer to project

--- index.js
+++ index.js
@@ -44,4 +44,16 @@ function todos (state = [], action) {
   }
 }
 
+function goals(state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+
+}
+
 const store = createStore(todos)
